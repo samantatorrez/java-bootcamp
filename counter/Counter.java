339f2ae6c9728663b503a5ecf6670267807b8310
Samanta Torrez <dsamantatorrez@gmail.com>
2015-01-18 03:41:46 -0300

    Update code practice 1.3
    practice 1.3 discounts

---
+++ counter/Counter.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package counter;
+
+/**
+ * @author Samanta Singleton Pattern what it does is creates a single instance
+ *         and sets the counter to zero . When getsecuence is called the counter
+ *         increases by one
+ */
+public class Counter {
+	private int secuence;
+
+	private static Counter counter = null;
+
+	private Counter() {
+		this.secuence = 0;
+	}
+
+	public static Counter getInstance() {
+		if (counter == null) {
+			counter = new Counter();
+		}
+		return counter;
+	}
+
+	public int getSecuense() {
+		secuence++;
+		return this.secuence;
+	}
+
+	public int getSecuence() {
+		return this.secuence;
+	}
+}
